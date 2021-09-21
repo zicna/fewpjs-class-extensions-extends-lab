e41608fc15ae1aaabd0bc576fef43a474279b72c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-21 13:41:15 -0500

    commit tree be1f73697fbe653116e7fe81efd461887e0e7f6e

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ class Polygon {
   }
 }
 class Triangle extends Polygon{
-    constructor(array){
-        super(array)
-    }
+    // constructor(array){
+    //     super(array)
+    // }
     get isValid(){
         if(this.countSides === 3 && this.array[0] + this.array[1] > this.array[2] && this.array[1] + this.array[2] > this.array[0] && this.array[0] + this.array[2] > this.array[1]) return true
         else return false
@@ -22,9 +22,9 @@ class Triangle extends Polygon{
 }
 
 class Square extends Polygon{
-    constructor(array){
-        super(array)
-    }
+    // constructor(array){
+    //     super(array)
+    // }
 
     get isValid(){
         if(this.countSides === 4 && this.array[0]=== this.array[1] && this.array[0]=== this.array[2] && this.array[0]=== this.array[3]) return true
